248e3f71d4478c3eb51277217a8023730d39138c
yichuancq <1012027293@qq.com>
2018-10-15 04:12:06 +0800

    add 文件记录写入mongo

--- words-cloud-core/src/main/scala/TFIDFDemo.scala
+++ words-cloud-core/src/main/scala/TFIDFDemo.scala
@@ -1,12 +1,9 @@
 import com.mongodb.spark.MongoSpark
 import org.ansj.recognition.impl.StopRecognition
 import org.ansj.splitWord.analysis.ToAnalysis
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext}
-
-import scala.collection.mutable
-import scala.io.Source
-
+import java.util.{ArrayList => JavaList}
+import scala.io.Source // rename
 /**
   * 中文词语特征值转换
   **/
@@ -14,7 +11,6 @@ object TFIDFDemo {
 
   //环境
   System.setProperty("hadoop.home.dir", "D:\\hadoop-common-2.2.0-bin")
-
   val False: Boolean = False
 
   /** *
@@ -22,7 +18,7 @@ object TFIDFDemo {
     * @param val1
     * @param val2
     */
-  private class MyWord(val val1: String, val val2: Integer) {
+  private class MyWord(val val1: String, val val2: Integer) extends Serializable {
 
     var filed: String = val1
     var weigh: Integer = val2
@@ -55,27 +51,28 @@ object TFIDFDemo {
       filter.insertStopWords(x.toString())
     }
     val splited = line.map(x => ToAnalysis.parse(x).recognition(filter).toStringWithOutNature(" "))
-    val wordCount = splited.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).map(x => (x._2, x._1)).sortByKey(ascending = False)
+    val wordCount = splited.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).map(x => (x._2, x._1))
+      .sortByKey(ascending = False)
+
     // 将RDD的前n个元素转换为数组返回
-    val chooseArray = wordCount.take(5000)
-    // 输出所有数组元素
-    //  val listResult = chooseArray.toList
-    val x = new mutable.HashSet[MyWord]()
+    val chooseArray = wordCount.take(100)
+    // java list
+    val myWordList = new JavaList[MyWord]()
+    // val mutableList = new mutable.MutableList[MyWord]()
     for (i <- 0 to (chooseArray.length - 1)) {
       //取程度大于2的值
       if (chooseArray(i)._2.length >= 2) {
         val myWord = new MyWord(chooseArray(i)._2, chooseArray(i)._1)
-        println(myWord.toString)
-        x.add(myWord)
+        myWordList.add(myWord)
       }
     }
-    val schema = StructType(Array(StructField("filed", StringType, true), StructField("weigh", IntegerType, true)))
-    //step1: 从原来的 RDD 创建一个行的 RDD
-
     import org.bson.Document
-    //val documents = sc.parallelize((x.toList).map(i => Document.parse(s"{test: $i}")))
-    val documents = sc.parallelize((1 to 10).map(i => Document.parse(s"{test: $i}")))
-    MongoSpark.save(documents) // Uses the SparkConf for configuration
+    //https://docs.mongodb.com/spark-connector/master/scala/write-to-mongodb/
+    val documents = sc.parallelize(
+      //
+      Seq(new Document("fruits", List(myWordList.toString).mkString(",")))
+    )
+    MongoSpark.save(documents)
 
     val rdd = MongoSpark.load(sc)
     println(rdd.count)
